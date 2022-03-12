98ed69df7b96a892cf49431caa93bec6b78c7492
maybecake <maybemaybecake@gmail.com>
2022-03-12 08:21:06 -0800

    Expand words.

--- read/read.js
+++ read/read.js
@@ -12,20 +12,26 @@ const BOOK = [
     'why is a bat in the bag ?',
     'the man is in the fast van',
     'is this a map to the lab ?',
-    'I have the ham in the pan .'
+    'I have the ham in the pan .',
 ]
-const SENTENCE = BOOK[0].split(' ');
+
+const FIRST_WORDS = [
+    'the of and a to in is you that it he was for on are as with his they i',
+    'at be this have from or one had by words but not what all were we when your can said',
+    'there use an each which she do how their if will up other about out many then them these so',
+    'some her would make like him into time has look two more write go see number no way could people',
+    'my than first water been called who am its now find long down day did get come made may part',
+]
+
+const SHORTS = {
+    'a': 'cat mat sat hat rat bat can man bad ran lad sad had fad tad',
+    'e': 'bed led fed pen den men ten let net wet pet get set bet yet',
+    'i': 'fit wit hit lit sit tin kin win pin pit big jig pig rig',
+    'o': 'cot pot got hot lot rod pod cod log dog fog hog rob',
+    'u': 'fun run sun pun rut gun hut nut shut cut mud tug rub tab grub',
+}
+
 const PUNCTUATION = ',.?!';
-
-// Set up grammer with words from sentence.
-const uniqueWords = [...new Set(SENTENCE)].filter((w) =>
-    !PUNCTUATION.includes(w));
-const grammer = '#JSGF V1.0; grammar words; public <word> = ' +
-    uniqueWords.join(` | `) + ' ;';
-console.log('grammer:', grammer);
-
-const grammerList = new SpeechGrammarList();
-grammerList.addFromString(grammer, 1);
 
 const CONFIDENCE_THRESHOLD = 0.6;
 
@@ -36,61 +42,13 @@ let activeCards = [];
 // Current custom sentence.
 let activeCustom;
 
-let rec = new SpeechRecognition();
-rec.continuous = true;
-rec.lang = 'en-US';
-rec.interimResults = true;
-rec.maxAlternatives = 1;
+// Main SpeechRecognition object
+let rec;
 
 const getActiveCard = () => {
     if (activeIndex >= 0 && activeIndex < activeCards.length)
         return activeCards[activeIndex];
     return;
-}
-
-rec.onresult = (e) => {
-    console.log('speech results.', e);
-
-    const results = [];
-    for (let i = 0; i < e.results.length; i++) {
-        const res = e.results[i];
-        for (let j = 0; j < res.length; j++) {
-            console.log(`${i},${j}`, res[j].confidence, res[j].transcript)
-            if (res[j].confidence > CONFIDENCE_THRESHOLD)
-                results.push(res[j].transcript);
-        }
-    }
-
-    console.log('results:', results);
-    if (results.length == 0) return;
-
-    const activeCard = getActiveCard();
-    console.log('activeCard?', activeCard, activeIndex);
-    if (!activeCard) return;
-
-    const text = activeCard.word;
-
-    for (r of results) {
-        console.log(text, 'in', r, '?');
-        if (r.includes(text)) {
-            activeCard.className = '';
-            activeCard.classList.add('card', 'correct');
-            // rec.stop();
-            // console.log('stopped!');
-            activateNext();
-        }
-    }
-}
-
-rec.onspeechend = (e) => {
-    console.log('speech ended.', e);
-    const activeCard = getActiveCard();
-    if (activeCard)
-        activeCard.classList.replace('active', 'wrong');
-}
-
-rec.onnomatch = (e) => {
-    console.log('no match!');
 }
 
 const activateNext = () => {
@@ -160,12 +118,75 @@ const createSentence = (container, sentenceId, sentence, cards) => {
     activeIndex = -1;
 };
 
-const createBook = () => {
+const createBook = (words) => {
+    // Set up grammar with words from sentence.
+    const uniqueWords = [...new Set(words)].filter((w) =>
+        !PUNCTUATION.includes(w));
+    const grammar = '#JSGF V1.0; grammar words; public <word> = ' +
+        uniqueWords.join(` | `) + ' ;';
+    console.log('grammar:', grammar);
 
+    const grammarList = new SpeechGrammarList();
+    grammarList.addFromString(grammar, 1);
+
+    rec = new SpeechRecognition();
+    rec.grammars = grammarList;
+    rec.continuous = false;
+    rec.lang = 'en-US';
+    rec.interimResults = false;
+    rec.maxAlternatives = 1;
+
+    rec.onresult = (e) => {
+        console.log('speech results.', e);
+
+        const results = [];
+        for (let i = 0; i < e.results.length; i++) {
+            const res = e.results[i];
+            for (let j = 0; j < res.length; j++) {
+                console.log(`${i},${j}`, res[j].confidence, res[j].transcript)
+                if (res[j].confidence > CONFIDENCE_THRESHOLD)
+                    results.push(res[j].transcript);
+            }
+        }
+
+        console.log('results:', results);
+        if (results.length == 0) return;
+
+        const activeCard = getActiveCard();
+        console.log('activeCard?', activeCard, activeIndex);
+        if (!activeCard) return;
+
+        const text = activeCard.word;
+
+        for (r of results) {
+            console.log(text, 'in', r, '?');
+            if (r.includes(text)) {
+                activeCard.className = '';
+                activeCard.classList.add('card', 'correct');
+                // rec.stop();
+                // console.log('stopped!');
+                activateNext();
+            }
+        }
+    }
+
+    rec.onspeechend = (e) => {
+        console.log('speech ended.', e);
+        const activeCard = getActiveCard();
+        if (activeCard)
+            activeCard.classList.replace('active', 'wrong');
+    }
+
+    rec.onnomatch = (e) => {
+        console.log('no match!');
+    }
+
+    createSentence(document.getElementById('main'), '0', words, activeCards);
 };
 
 window.onload = () => {
-    createSentence(document.getElementById('main'), '0', SENTENCE, activeCards);
+    const allShort = Object.values(SHORTS).reduce((a, b) => a + ' ' + b);
+    createBook((FIRST_WORDS[0] + ' ' + allShort).split(' '));
 
     document.getElementById('listen').onclick = () => {
         activateNext();
